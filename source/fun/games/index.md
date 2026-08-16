2b948c3778ca0e59b4cd63e4b335135daa1baf13
Mu An <libohang1228@163.com>
2026-08-16 17:05:49 +0800

    娱乐菜单改下拉（音乐/电影/游戏）+ 电影游戏加海报封面

--- source/fun/games/index.md
+++ source/fun/games/index.md
@@ -2,14 +2,4 @@
 title: 游戏
 ---
 
-<div class="media-grid">
-  <div class="media-card"><span class="media-name">赛博朋克 2077</span><span class="media-sub">RPG</span></div>
-  <div class="media-card"><span class="media-name">侠盗猎车手 V</span><span class="media-sub">开放世界</span></div>
-  <div class="media-card"><span class="media-name">侠盗猎车手：圣安地列斯</span><span class="media-sub">开放世界</span></div>
-  <div class="media-card"><span class="media-name">生死狙击</span><span class="media-sub">FPS</span></div>
-  <div class="media-card"><span class="media-name">王者荣耀</span><span class="media-sub">MOBA</span></div>
-  <div class="media-card"><span class="media-name">黑暗之魂</span><span class="media-sub">魂游</span></div>
-  <div class="media-card"><span class="media-name">艾尔登法环</span><span class="media-sub">魂游</span></div>
-  <div class="media-card"><span class="media-name">只狼：影逝二度</span><span class="media-sub">魂游</span></div>
-  <div class="media-card"><span class="media-name">血源诅咒</span><span class="media-sub">魂游</span></div>
-</div>
+<div id="game-grid" class="media-grid">加载中…</div>
---
+++ source/js/media-grid.js
@@ -0,0 +1,40 @@
+// ===== 电影/游戏网格渲染（读取 /data/movies.json、/data/games.json）=====
+(function () {
+  function render(mountId, dataUrl) {
+    var mount = document.getElementById(mountId);
+    if (!mount) return;
+    fetch(dataUrl)
+      .then(function (r) { return r.json(); })
+      .then(function (items) {
+        mount.innerHTML = items.map(function (item) {
+          var name = item.name, sub = item.sub, img = item.img;
+          if (img) {
+            return '<div class="media-card">' +
+              '<img src="' + img + '" alt="' + name + '" loading="lazy">' +
+              '<span class="media-name">' + name + '</span>' +
+              '<span class="media-sub">' + sub + '</span>' +
+              '</div>';
+          }
+          return '<div class="media-card media-noimg">' +
+            '<div class="media-placeholder"><i class="fas fa-image"></i></div>' +
+            '<span class="media-name">' + name + '</span>' +
+            '<span class="media-sub">' + sub + '</span>' +
+            '</div>';
+        }).join('');
+      })
+      .catch(function () {
+        mount.innerHTML = '<p style="color:#999;text-align:center;">加载失败</p>';
+      });
+  }
+
+  function init() {
+    render('movie-grid', '/data/movies.json');
+    render('game-grid', '/data/games.json');
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
