cd9cc2e043889b6659f38ac4fccd0eb5cf9f141c
Mu An <libohang1228@163.com>
2026-09-08 01:28:50 +0800

    fix(finance): 去掉东方财富fetch的Referer头(forbidden header会触发CORS预检) + ?v=9

--- source/js/finance-tracker.js
+++ source/js/finance-tracker.js
@@ -590,7 +590,7 @@
     var results = await Promise.all(chartReqs.map(function (item) {
       var url = 'https://push2his.eastmoney.com/api/qt/stock/kline/get?secid=' + item.secid +
         '&fields1=f1,f2,f3,f4,f5,f6&fields2=f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61&klt=101&fqt=1&beg=20260601&end=20500101';
-      return fetch(url, { headers: { 'Referer': 'https://quote.eastmoney.com/' } })
+      return fetch(url)
         .then(function (resp) { return resp.json(); })
         .then(function (data) {
           var converted = convertEastMoneyKline(data);
